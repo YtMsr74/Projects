2ad0402466b4d528cc424871d133c7385cf50d72
YtMsr74 <125552741+YtMsr74@users.noreply.github.com>
2023-02-24 21:30:22 +0700

    function values calculation

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	float x, st, step;
+	printf("Enter the starting point ( (0;4] )\n");
+	scanf_s("%f", &st);
+	printf("x y\n");
+	step = (4 - st) / 10;
+	for (x = st;x <= 4;x += step) printf("%.2f %.4f\n", x, ((sin(x)) / x));
+}
