5e0428e348209f65bfe91bab3b320ec851073b24
YtMsr74 <125552741+YtMsr74@users.noreply.github.com>
2023-03-10 20:12:54 +0700

    Update 9.4 Char Array.cpp

--- 9.4 Char Array.cpp
+++ 9.4 Char Array.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 {
     int n,i;
-    char* s[20][11];
+    char s[20][11];
     cout << "Enter an amount of words (up to 20) ";
     cin >> n;
     cout << "Enter your words (max symbols: 10)" << endl;
-    for (i = 0;i < n;i++) scanf("%s", *s[i]);
+    for (i = 0;i < n;i++) cin >> s[i];
+    cout << endl;
     for (i = 1;i < n;i += 2) cout << s[i] << endl;
-    cout << s[1] << endl;
 }
